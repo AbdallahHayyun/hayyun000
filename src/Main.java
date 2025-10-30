94ee63dbc1d2f74b0aa71677accde94cee526167
Hayyun <hayyun06@gmail.com>
2025-10-30 15:02:19 +0700

    readme

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        // Input dua bilangan
         System.out.print("Masukkan bilangan pertama: ");
         double bil1 = input.nextDouble();
 
@@ -14,7 +13,6 @@ public class Main {
         double hasilTambah = bil1 + bil2;
         double hasilKurang = bil1 - bil2;
 
-        // Output hasil
         System.out.println("\n=== Hasil Perhitungan ===");
         System.out.println("Penjumlahan: " + bil1 + " + " + bil2 + " = " + hasilTambah);
         System.out.println("Pengurangan: " + bil1 + " - " + bil2 + " = " + hasilKurang);
